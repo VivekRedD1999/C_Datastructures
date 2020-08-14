0755692161e6b5df3cd1959b1c9d754cce7e798f
VivekRedD1999 <58820599+VivekRedD1999@users.noreply.github.com>
2020-08-15 00:50:58 +0530

    Create QueueImplementation.c

---
+++ QueueImplementation.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<conio.h>
+int maxsize=10;
+int queue[10];
+int top=0,item;
+
+int enqueue(int item)
+{
+    if (!isempty())
+    {
+        queue[top]=item;
+        top=top+1;
+        printf("pushed element is %d\n",item);
+        return item;
+    }
+    else
+    {
+        printf("queue is full");
+    }       
+}
+int dequeue()
+{
+    if (!isempty())
+    {
+        item=queue[0];
+        top=top-1;
+        printf("popped element is %d\n",item);
+        return item;
+    }
+    else
+    {
+        printf("the queue is empty");
+    }
+}
+int isempty()
+{
+    if (top==-1)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }    
+}
+int isfull()
+{
+    if (top==maxsize)
+    {
+        return 1;
+        //printf("queue is full");
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+int main()
+{
+    enqueue(3);
+    enqueue(4);
+    enqueue(5);
+    printf("%s\n",isempty()?"true":"false");
+    printf("%s\n",isfull()?"true":"false");
+    dequeue();
+    
+}
